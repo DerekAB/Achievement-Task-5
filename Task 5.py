43d90cbe3edbb207d322a7a5cf4c2001c5a79b03
DerekAB <123401143+DerekAB@users.noreply.github.com>
2023-02-23 19:15:53 +0000

    finished

--- Task 5.py
+++ Task 5.py
@@ -7,3 +7,12 @@
 #
 #The purpose of this program is to generate and then cube numbers 1-10
 
+numbers = {}
+#defining the variable as a dictionary
+
+for x in range(1, 11):
+    numbers[x] = x ** 3
+#creating a for loop to generate the numbers 1-10 and then cubing the numbers it generates
+    
+print(numbers)
+#printing the result
