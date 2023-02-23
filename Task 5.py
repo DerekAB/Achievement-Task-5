c7c80e495d233dcbb665a3c24c822968de93345d
DerekAB <123401143+DerekAB@users.noreply.github.com>
2023-02-23 19:04:01 +0000

    beginning

---
+++ Task 5.py
@@ -0,0 +1,9 @@
+#Name:                  Task 5.py
+#Author:                Derek Baker
+#Date Created:          23-02-2023
+#Date Last Modified:    23-02-2023
+#
+#Purpose:
+#
+#The purpose of this program is to generate and then cube numbers 1-10
+
